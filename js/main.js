ef5895df330174e0f271bc48232601b8e602c1c4
damianolagana <damianolagana@gmail.com>
2023-04-17 16:43:54 +0200

    Buzz multiplo 5

--- js/main.js
+++ js/main.js
@@ -6,8 +6,10 @@ for(let i = 1; i <= 100; i++){
 
     if(i % 3 == 0){
         risultato = "Fizz"
+    } else if (i % 5 == 0){
+       risultato = "Buzz"
     } else {
-       
+        
     }
 
     console.log(risultato)
