7e99978b162b296091b5b44ae7dc33fc8d45b743
damianolagana <damianolagana@gmail.com>
2023-04-18 11:12:39 +0200

    Prova Bonus 2

--- js/main.js
+++ js/main.js
@@ -4,24 +4,29 @@ const contenitore = document.getElementById("container")
 for(let i = 1; i <= 100; i++){
     
     let risultato = i
+    let classe = "";
 
-    if(i % 3 == 0){
-        risultato = "Fizz"
-    } else if (i % 5 == 0){
-       risultato = "Buzz"
-    }  
-    
     if(i % 3 == 0 && i % 5 == 0){
         risultato ="FizzBuzz"
+        classe = "blu";
+    } else if (i % 5 == 0){
+       risultato = "Buzz"
+       classe = "rosso"
+    } else if (i % 3 == 0){
+       risultato = "Fizz"
+       classe = "verde" 
     }
+    
 
     console.log(risultato)
 
-    // contenitore.innerHTML += `<div class="box">${risultato}</div>`;
+    contenitore.innerHTML += `<div class="box ${classe}">${risultato}</div>`;
 
-    const nuovoElementoDiv = document.createElement('div'); 
-    nuovoElementoDiv.append(risultato); 
-    contenitore.append(nuovoElementoDiv); 
-
-    nuovoElementoDiv.innerHTML = `<div class="box">${risultato}</div>`;
+    
 }
+
+const box = document.querySelector(".box")
+
+console.log(box.style)
+
+box.style.backgroundcolor = 'green'
